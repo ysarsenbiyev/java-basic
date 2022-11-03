1f36a74d097fa129c15f3d7872397464127549c3
Yerbol <sarsenbiev01@gmail.com>
2022-11-03 15:10:29 +0600

    Домашнее задание

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, мир! Я изучаю Java.");
+        // Однострочный комментарий
+        /* Многострочный
+        комментарий
+         */
+    }
+}
